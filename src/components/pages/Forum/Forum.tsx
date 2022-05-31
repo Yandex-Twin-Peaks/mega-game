fc22c292190f04d317b34ae3db3a8a25c5314f55
Anna Danshina <annetteblackat@gmail.com>
2022-05-31 19:08:21 +0300

    добавила роутинг

---
+++ src/components/pages/Forum/Forum.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Forum = () => {
+  return (
+    <h1>FORUM</h1>
+  );
+};
+
+export default Forum;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Main from './Main';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 
 import './styles/index.pcss';
 import './styles/normalize.css';
 
 const container = document.getElementById('root')!;
+
+const app = (
+  <HashRouter>
+    <Main />
+  </HashRouter>
+);
+
 const root = createRoot(container);
-root.render(<Main />);
+root.render(app);
