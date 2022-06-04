2df2a2a657bc07b0286b691d83e28e1df6931252
Dima <saykind93@mail.ru>
2022-06-04 18:27:14 +0300

    Добавил компонент Ошибка и Error Boundler

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Main from './Main';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+import Error from './components/atoms/Error';
 
 import './styles/index.pcss';
 import './styles/normalize.css';
@@ -9,9 +11,16 @@ import './styles/normalize.css';
 const container = document.getElementById('root')!;
 
 const app = (
-  <HashRouter>
-    <Main />
-  </HashRouter>
+  <ErrorBoundary
+    FallbackComponent={Error}
+    onReset={() => {
+      // reset the state of your app so the error doesn't happen again
+    }}
+  >
+    <HashRouter>
+      <Main />
+    </HashRouter>
+  </ErrorBoundary>
 );
 
 const root = createRoot(container);
