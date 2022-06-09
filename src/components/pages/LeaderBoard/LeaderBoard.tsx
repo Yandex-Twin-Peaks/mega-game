b1e1d87ebb862bdb1f07c76cb09ec6866905be05
Yury Lunev <xalt@mail.ru>
2022-06-09 20:09:55 +0300

    Add VK UI Package. Install with  --legacy-peer-deps flag!

--- src/components/pages/LeaderBoard/LeaderBoard.tsx
+++ src/components/pages/LeaderBoard/LeaderBoard.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import {
+  useAdaptivity,
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  ViewWidth,
+  View,
+  Panel,
+  PanelHeader,
+  Header,
+  Group,
+  SimpleCell,
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
 
 const LeaderBoard = () => {
+  const { viewWidth } = useAdaptivity();
+
   return (
-    <h1>LEADER BOARD</h1>
+    <AppRoot>
+      <SplitLayout header={<PanelHeader separator={false}/>}>
+        <SplitCol spaced={viewWidth > ViewWidth.MOBILE}>
+          <View activePanel='main'>
+            <Panel id='main'>
+              <PanelHeader>LEADER BOARD</PanelHeader>
+              <Group header={<Header mode='secondary'>Items</Header>}>
+                <SimpleCell>Hello</SimpleCell>
+                <SimpleCell>World</SimpleCell>
+              </Group>
+            </Panel>
+          </View>
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
   );
 };
 
