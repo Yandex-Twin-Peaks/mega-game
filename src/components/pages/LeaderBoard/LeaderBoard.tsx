39c42ad3764e6864a1a0c4e3c7bf2623ebc6fcc2
Yury Lunev <xalt@mail.ru>
2022-06-12 00:25:17 +0300

    Add Material UI Package

--- src/components/pages/LeaderBoard/LeaderBoard.tsx
+++ src/components/pages/LeaderBoard/LeaderBoard.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 const LeaderBoard = () => {
   return (
-    <h1>LEADER BOARD</h1>
+    <>
+      <h1>LEADER BOARD</h1>
+      <Button variant='contained' color='primary'>
+        Hello World
+      </Button>
+    </>
   );
 };
 
