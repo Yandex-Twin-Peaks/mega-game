ed0e095d94772d7e02a4e618c73f93d993de2f75
Dima <saykind93@mail.ru>
2022-06-11 16:40:11 +0300

    долбежка с муи

--- src/components/organisms/NavBar/NavBar.tsx
+++ src/components/organisms/NavBar/NavBar.tsx
@@ -1,46 +1,70 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import './NavBar.pcss';
-import { GiGamepad } from 'react-icons/gi';
+import { AppBar } from '@material-ui/core';
 
 
 function NavBar() {
 
-  return (
-    <nav className='Nav'>
-      <div className='Nav__container'>
-        <Link to='/' className='Nav__brand'>
-          <GiGamepad style={{
-            height: '80px',
-            width: '80px'
-          }} />
-        </Link>
 
-        <div className='Nav__right'>
-          <ul className='Nav__item-wrapper'>
-            <li className='Nav__item'>
-              <Link className='Nav__link' to='/home'>home</Link>
-            </li>
-            <li className='Nav__item'>
-              <Link className='Nav__link' to='/signin'>signin</Link>
-            </li>
-            <li className='Nav__item'>
-              <Link className='Nav__link' to='/forum'>forum</Link>
-            </li>
-            <li className='Nav__item'>
-              <Link className='Nav__link' to='/leaderboard'>leaderboard</Link>
-            </li>
-            <li className='Nav__item'>
-              <Link className='Nav__link' to='/game'>game</Link>
-            </li>
-            <li className='Nav__item'>
-              <Link className='Nav__link' to='/profile'>profile</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  // const PAGES = [
+  //   '/',
+  //   '/home',
+  //   '/signin',
+  //   '/forum',
+  //   '/leaderboard',
+  //   '/game',
+  //   '/profile'
+  // ];
+
+  return (
+  // <AppBar>
+  //   <Toolbar>
+
+
+  //     {/* <Link to='/' className='Nav__brand'>
+  //       <GiGamepad style={{
+  //         height: '80px',
+  //         width: '80px'
+  //       }} />
+  //     </Link> */}
+
+  //     {/* <Tabs textColor='inherit'>
+  //         {PAGES.map((page, index) => (<Tab key={index} label={page}/> ))}
+
+  //       </Tabs> */}
+
+  //     {/* <div className='Nav__right'>
+  //       <ul className='Nav__item-wrapper'>
+  //         <li className='Nav__item'>
+  //           <Link className='Nav__link' to='/home'>home</Link>
+  //         </li>
+  //         <li className='Nav__item'>
+  //           <Link className='Nav__link' to='/signin'>signin</Link>
+  //         </li>
+  //         <li className='Nav__item'>
+  //           <Link className='Nav__link' to='/forum'>forum</Link>
+  //         </li>
+  //         <li className='Nav__item'>
+  //           <Link className='Nav__link' to='/leaderboard'>leaderboard</Link>
+  //         </li>
+  //         <li className='Nav__item'>
+  //           <Link className='Nav__link' to='/game'>game</Link>
+  //         </li>
+  //         <li className='Nav__item'>
+  //           <Link className='Nav__link' to='/profile'>profile</Link>
+  //         </li>
+  //       </ul>
+  //     </div> */}
+  //   </Toolbar>
+  // </AppBar>
+
+    // <>
+    //   <AppBar>
+    //     хер
+    //   </AppBar>
+    // </>
+    <div>fff</div>
   );
 }
 
