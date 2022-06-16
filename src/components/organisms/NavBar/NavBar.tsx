ba9db2be3968bd1f1915bf1ed9c7c12ad1ed21fe
Анна Даньшина <annablackat@gmail.com>
2022-06-17 01:30:53 +0300

    переделала NavBar

--- src/components/organisms/NavBar/NavBar.tsx
+++ src/components/organisms/NavBar/NavBar.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 import { routes } from '../../../router/config';
-import './NavBar.pcss';
 import { GiGamepad } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
 import {
   AppBar, Tab, Tabs, Toolbar, Button
 } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { sendUserLogOutPending } from '../../../_store/actions/auth.actions';
+
+import './NavBar.pcss';
 
 const newLocal = '#063970';
 
 function NavBar() {
-
+  const dispatch = useDispatch();
   const [value, setPartMenu] = useState(0);
 
+  const filteredPaths = routes.filter(pathItem => pathItem.pageName !== 'Auth');
 
   return (
     <>
@@ -24,14 +28,19 @@ function NavBar() {
             onChange={(e, value) => setPartMenu(value)}
             indicatorColor='secondary'
           >
-            {routes.map((conf, index) => (index === 0 ? <Tab key={index} label={conf.pageName} icon={ <GiGamepad style={{
+            {filteredPaths.map((conf, index) => (index === 0 ? <Tab key={index} label={conf.pageName} icon={ <GiGamepad style={{
               height: '80px',
               width: '80px'
             }} />} component={Link} to={conf.path} sx = {{ marginRight: '100px' }} /> :
               <Tab key={index} label={conf.pageName} component={Link} to={conf.path} />))}
           </Tabs>
-          <Button sx={{ marginLeft: 'auto' }} variant='contained'>Login{' '}</Button>
-          <Button sx={{ marginLeft: '10px' }} variant='contained'>Registry{' '}</Button>
+          <Button
+            sx={{ marginLeft: 'auto' }}
+            variant='contained'
+            onClick={() => dispatch(sendUserLogOutPending())}
+          >
+            Log out{' '}
+          </Button>
         </Toolbar>
       </AppBar>
     </>
